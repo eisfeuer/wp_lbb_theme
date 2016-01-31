2bf7cd6ee63c17ff5ffeea61d456f3af852d6731
Niclas <nschirrmeister@web.de>
2016-01-31 23:42:58 +0100

    add menu

--- config/initializers/menus.php
+++ config/initializers/menus.php
@@ -9,8 +9,7 @@ function register_custom_menus() {
   /*
    * Place here all your register_nav_menu() calls.
    */
-
-  //register_nav_menu('main_menu', 'Header Menu');
+  register_nav_menu('main_menu', 'Header Menu');
 }
 
 add_action('init', 'register_custom_menus');
